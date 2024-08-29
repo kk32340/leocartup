2daac2341241cefa24001e332b82d1d8a6eb23a3
Kalimuthu <k.kalimuthu@gmail.com>
2024-08-29 10:19:10 +0530

    Api updated

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Define a route
-app.get('/', (req, res) => {
-  //res.send('Hello, world! This is my Node.js app.');
-  res.sendFile(__dirname + '/index.html');
+app.get('/api', (req, res) => {  
+  res.send('Api call');
 });
 
+app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
