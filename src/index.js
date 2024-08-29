022169a35a34bc022b444dff7c95fc895cda09aa
Kalimuthu <k.kalimuthu@gmail.com>
2024-08-29 10:00:47 +0530

    web site deployment

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ const port = process.env.PORT || 3000;
 
 // Define a route
 app.get('/', (req, res) => {
-  res.send('Hello, world! This is my Node.js app.');
+  //res.send('Hello, world! This is my Node.js app.');
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Start the server asynchronously
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
